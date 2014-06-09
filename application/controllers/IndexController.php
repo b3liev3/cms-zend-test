a019068e6d3e4cf7b6574297e8989f5b0fd6e7e6
b3liev3 <pere@soms.cat>
2014-06-09 16:41:25 +0200

    Tests

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,6 +11,24 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        echo "<pre>";
+        echo print_r($_REQUEST,true);
+        echo "</pre>";
+        
+        setcookie("message","no problemo");
+        
+        if(isset($_COOKIE['message'])){
+            echo $_COOKIE['message'].'<br/>';
+        }
+        
+        session_start();
+        if(!isset($_SESSION['count'])){
+            $_SESSION['count'] = 0;
+        }else{
+            $_SESSION['count']++;
+        }
+        echo 'counter: '.$_SESSION['count'].'<br/>';
+           
     }
 
 
